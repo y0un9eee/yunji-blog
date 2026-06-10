d6c1dee1110b449781946f3befd9954d1ac67494
y0un9eee <natekimyoungee9@gmail.com>
2026-06-10 22:48:48 +0900

    fix: Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
